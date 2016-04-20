71ef581f6b36cff1461f62b20ad29cfafe357833
Nathan Venn <natevenn@gmail.com>
2016-04-20 09:06:36 -0600

    seed database with states and thier install count

--- app/models/database_updater.rb
+++ app/models/database_updater.rb
@@ -1,6 +1,4 @@
 class DatabaseUpdater
-  #include UsStates
-
   attr_reader :installs_by_state, :us_states
 
   def initialize
@@ -9,13 +7,18 @@ class DatabaseUpdater
   end
 
   def install_count(state)
-    install_by_state.install_count_by_state(state)
+    installs_by_state.install_count_by_state(state)
   end
 
   def update_states_with_install_totals
     us_states.each do |state|
       state = State.find_by(name: state.last)
-      state.update(installs: install_count(state.last)
+      state.update(installs: install_count(state.last))
+      require "pry"
+      binding.pry
     end
   end
 end
+
+db = DatabaseUpdater.new
+db.update_states_with_install_totals
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
 class Seed
-
   def initialize
     generate_states
   end
 
   def generate_states
+    count = 0
     UsStates.all.each do |state|
-      State.create!(name: state.last, installs: install_count(state.last)
+      count += 1
+      State.create!(name: state.last, installs: install_count(state.last))
+      puts "#{state.last} created, count: #{count}"
     end
   end
 
@@ -14,3 +16,5 @@ class Seed
     InstallSummary.new.install_count_by_state(state)
   end
 end
+
+Seed.new
