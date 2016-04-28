ab21737a151b500217cb1dc102a42f357e194c3c
Nathan Venn <natevenn@gmail.com>
2016-04-28 09:53:04 -0600

    add test coverage for welcome controller

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,6 @@ require 'simplecov'
 SimpleCov.start 'rails'
 
 RSpec.configure do |config|
-  #config.before(:suite) do
-    #begin
-      #DatabaseCleaner.start
-    #ensure
-      #DatabaseCleaner.clean
-    #end
-  #end
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
